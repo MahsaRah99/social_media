509cdcea3ec0f7bced8fc2d16a0b6ae2cefd48af
Mahsa R <mahsarm99@gmail.com>
2023-10-18 14:53:48 +0330

    create BaseModel

--- core/models.py
+++ core/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+from uuid import uuid4
+from django.db.models.query import QuerySet
+from django.utils.translation import gettext_lazy as _
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    
+    class Meta:
+        abstract = True
