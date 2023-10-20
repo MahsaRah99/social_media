f59500cc22eac265676cc6c633e5d823ad34ab5d
Mahsa R <mahsarm99@gmail.com>
2023-10-20 22:32:57 +0330

    create Tag model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from core.models import BaseModel, SoftDeleteModel, TimeStampMixin
+from django.utils.translation import gettext_lazy as _
+from Blaze.settings import AUTH_USER_MODEL
+from django.urls import reverse
+from django.db.models import Manager
 
-# Create your models here.
+
+class Tag(BaseModel):
+    name = models.CharField(verbose_name=_("name"), max_length=20, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def tag_post_count(self):
+        return self.posts.count()
+
