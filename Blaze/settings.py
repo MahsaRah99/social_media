6e6be0112e7a7fe20832625bbf6c5d053a1e26b7
Mahsa Rahimi <133171171+MahsaRah99@users.noreply.github.com>
2023-12-26 13:20:29 +0330

    edit email host settings

--- Blaze/settings.py
+++ Blaze/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,8 +136,8 @@ LOGIN_URL = '/users/login/'
 #Google account
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'annayee3017@gmail.com'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'otfrpetveprsrfvj'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Blaze Website'
