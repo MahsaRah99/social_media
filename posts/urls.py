32d220ed7beff6a14feb77cd231d1a285c9ed810
Mahsa R <mahsarm99@gmail.com>
2023-12-27 12:06:29 +0330

    chang int to uuid in urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,25 @@
 from django.urls import path
-from users import views
+from . import views
 
-app_name = 'posts'
+app_name = "posts"
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
+    path("home/", views.HomeView.as_view(), name="home"),
+    path(
+        "post/<uuid:post_id>/<slug:post_slug>/",
+        views.PostDetailView.as_view(),
+        name="post_detail",
+    ),
+    path(
+        "post/delete/<uuid:post_id>/", views.PostDeleteView.as_view(), name="post_delete"
+    ),
+    path(
+        "post/update/<uuid:post_id>/", views.PostUpdateView.as_view(), name="post_update"
+    ),
+    path("post/create/", views.PostCreateView.as_view(), name="post_create"),
+    path(
+        "reply/<uuid:post_id>/<uuid:comment_id>/",
+        views.PostAddReplyView.as_view(),
+        name="add_reply",
+    ),
+    path("like/<uuid:post_id>/", views.PostLikeView.as_view(), name="post_like"),
 ]
